18665053613f7409bfe61082418a5fe2ed738281
pconner6 <pconner6@gatech.edu>
2024-10-30 16:06:51 -0400

    gitignore test

--- SpotifyWrapped/credentials.py
+++
@@ -1,8 +0,0 @@
-import os
-
-SPOTIFY_CLIENT_ID = '7d35aa773e9c47d1b49c8ae4c9ecc4f4'
-SPOTIFY_CLIENT_SECRET = '60686f44c374413eb66a1088d9d93b13'
-SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:8000/callback/'
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0=eydcga!-#_ddl0f$u*=v%cq#%b2j0aasn1rjwreho0f8r&x*'
