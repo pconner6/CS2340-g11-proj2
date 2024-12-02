e97504a2d55ae8f77299622b4c91063207bc3327
Breadfishman <farisafakhouri@gmail.com>
2024-12-02 04:44:20 +0000

    final prod fix

--- tailscale_funnel.sh
+++ tailscale_funnel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Kill other processes
+pkill -f tailscale_funnel.sh & nohup
+
 # Run the Server
 python3 manage.py runserver
 
-# Enable funnel on localhost:2001
+# Enable funnel on 8000
 tailscale funnel 8000
 
 # Keep the script running indefinitely
